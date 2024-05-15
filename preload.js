99a3929ab3af7ecc46f1cee71f3ae253eb9ea5d1
whr <wang.whr@alibaba-inc.com>
2024-05-15 18:46:05 +0800

    初始化

---
+++ preload.js
@@ -0,0 +1,20 @@
+// 通过 'content-security-policy' 头部来设置CSP
+self.addEventListener('fetch', event => {
+  if (event.request.method === 'GET') {
+    event.respondWith(
+      fetch(new Request(event.request.url, {
+        mode: 'no-cors',
+        redirect: 'follow',
+        referrer: 'https://term.ptt.cc',
+        headers: {
+          Origin: 'https://term.ptt.cc',
+        }
+      })).then(response => {
+        console.log('!!!!!!!!!!!!!!!')
+        return response;
+      }).catch(error => {
+        console.error('Fetch error:', error);
+      })
+    );
+  }
+});
